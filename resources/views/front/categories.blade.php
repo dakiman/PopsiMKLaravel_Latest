665d961a60231bb7bbaa3b379460342716566e5d
dakiman <dvancov@hotmail.com>
2018-03-07 18:38:50 +0100

    category display split into partial

--- resources/views/front/categories.blade.php
+++ resources/views/front/categories.blade.php
@@ -32,31 +32,7 @@
     <div class="col-md-9">
       @foreach ($categories as $category)
       <div class="row {{$category->id}} @if($category->id == $categories[0]["id"]) active-items @else inactive-items @endif">
-          <div class="col-md-11 mx-auto">
-              <div class="items-container card border-primary">
-                  <div class="media">
-                      <img class="mx-3 my-3" src="{{Storage::url('/categories/'.$category->picture)}}" alt="Generic placeholder image">
-                      <div class="media-body my-auto px-2">
-                          <h5 class="mt-0">{{$category->translateTitle()}}</h5>
-                          {{$category->translateDescription()}}
-                      </div>
-                  </div>
-                  <div class="card-body">
-                      <div class="row mx-auto">
-                          @foreach ($category->activeItems() as $item)
-                          <div class="col-md-3 px-1 py-1">
-                              <div class="card item-card text-center border-muted rounded mx-auto">
-                                  <img class="card-img-top" src="{{Storage::url('/items/'.$item->getCoverPhoto() )}}" alt="Card image cap">
-                                  <div class="card-body text-center">
-                                      <div class="card-title ">{{$item->translateTitle()}}</div>
-                                  </div>
-                              </div>
-                          </div>
-                          @endforeach
-                      </div>
-                  </div>
-              </div>
-          </div>
+          @include('partials.category-partial')
       </div>
       @endforeach
     </div>
---
+++ resources/views/partials/category-partial.blade.php
@@ -0,0 +1,27 @@
+<div class="col-md-11 mx-auto">
+    <div class="items-container card border-primary">
+        <div class="media">
+            <a href="/catalogue/categories/{{$category->id}}"><img class="mx-3 my-3" src="{{Storage::url('/categories/'.$category->picture)}}" alt="Generic placeholder image"></a>
+            <div class="media-body my-auto px-2">
+            <h5 class="mt-0">
+                <a href="/catalogue/categories/{{$category->id}}">{{$category->translateTitle()}}</a>
+            </h5>
+                {{$category->translateDescription()}}
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="row mx-auto">
+                @foreach ($category->activeItems() as $item)
+                <div class="col-md-3 px-1 py-1">
+                    <div class="card item-card text-center border-muted rounded mx-auto">
+                        <img class="card-img-top" src="{{Storage::url('/items/'.$item->getCoverPhoto() )}}" alt="Card image cap">
+                        <div class="card-body text-center">
+                            <div class="card-title ">{{$item->translateTitle()}}</div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
