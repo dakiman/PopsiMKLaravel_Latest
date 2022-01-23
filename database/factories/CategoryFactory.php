7f6703e4a601111f7fcf75938e975dc5928f1de1
dakiman <dvancov@hotmail.com>
2022-01-23 23:14:53 +0100

    updating

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,8 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
-    $images = ['fotografia_1.jpg', 'fotografia_2.jpg', 'fotografia_3.jpg', 'fotografia_4.jpg', 'lanec_4.jpg', 'lanec_5_1.jpg', 'lanec_6_1.jpg', 'lanec_7.jpg'];
-    return [
+//    $images = ['fotografia_1.jpg', 'fotografia_2.jpg', 'fotografia_3.jpg', 'fotografia_4.jpg', 'lanec_4.jpg', 'lanec_5_1.jpg', 'lanec_6_1.jpg', 'lanec_7.jpg'];
+//    $images = ['bearings.bmp'];
+	$images = ['bearings.bmp', 'codex_chains.bmp', 'codex1.png', 'codex4.bmp', 'codex8.bmp'];
+
+	return [
         'title_en' => ucfirst($faker->word),
         'title_mk' => ucfirst($faker->word),
         'description_en' => "EN_" . $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -3,8 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Item::class, function (Faker $faker) {
-    $images = ['fotografia_1.jpg', 'fotografia_2.jpg', 'fotografia_3.jpg', 'fotografia_4.jpg', 'lanec_4.jpg', 'lanec_5_1.jpg', 'lanec_6_1.jpg', 'lanec_7.jpg'];
-    shuffle($images);
+//    $images = ['fotografia_1.jpg', 'fotografia_2.jpg', 'fotografia_3.jpg', 'fotografia_4.jpg', 'lanec_4.jpg', 'lanec_5_1.jpg', 'lanec_6_1.jpg', 'lanec_7.jpg'];
+//	$images = ['bearings.bmp'];
+	$images = ['bearings.bmp', 'codex_chains.bmp', 'codex1.png', 'codex4.bmp', 'codex8.bmp'];
+
+	shuffle($images);
     return [
         'category_id' => 1,
         'title_en' => ucfirst($faker->word) . "_en",
