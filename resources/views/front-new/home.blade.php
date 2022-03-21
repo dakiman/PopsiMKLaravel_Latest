5e6c0f5a545436d2c378cc007c566023438b12e5
dakiman <dvancov@hotmail.com>
2022-03-22 00:20:18 +0100

    Revert "carousel only on desktop"
    This reverts commit 714de9ae9bd1b1cbfab18ab6d18d36664c1d85b5.

--- resources/views/front-new/home.blade.php
+++ resources/views/front-new/home.blade.php
@@ -1,7 +1,5 @@
 @extends('front-new.layout')
 @section('content')
-    @notmobile
     @include('front-new.carousel-partial')
-    @endnotmobile
     @include('front-new.products-partial')
 @endsection
