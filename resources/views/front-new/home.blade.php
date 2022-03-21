714de9ae9bd1b1cbfab18ab6d18d36664c1d85b5
dakiman <dvancov@hotmail.com>
2022-03-21 23:56:22 +0100

    carousel only on desktop

--- resources/views/front-new/home.blade.php
+++ resources/views/front-new/home.blade.php
@@ -1,5 +1,7 @@
 @extends('front-new.layout')
 @section('content')
+    @notmobile
     @include('front-new.carousel-partial')
+    @endnotmobile
     @include('front-new.products-partial')
 @endsection
