955e75b491115e795994317ef3a566efa74eb0e6
dakiman <dvancov@hotmail.com>
2022-07-06 13:59:03 +0200

    add cookie banner close button

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -2,9 +2,16 @@
     <div class="row">
         <div class="col-md-4 col-sm-12 button-fixed">
             <div class="p-3 pb-4 bg-custom text-white">
+                <div class="row">
+                    <div class="offset-10 col-2 text-center">
+                        <button type="button" class="close" aria-label="Close">
+                            <span aria-hidden="true" class="text-white">&times;</span>
+                        </button>
+                    </div>
+                </div>
                 <p class="text-white">{!! trans('cookieConsent::texts.message') !!}</p>
                 <button type="button" class="btn btn-primary w-100 js-cookie-consent-agree cookie-consent__agree">{{ trans('cookieConsent::texts.agree') }}</button>
-                <button type="button" class="btn btn-dark bg-transparent w-100 mt-1 js-cookie-consent-agree cookie-consent__agree">{{ trans('cookieConsent::texts.disagree') }}</button>
+{{--                <button type="button" class="btn btn-dark bg-transparent w-100 mt-1 js-cookie-consent-agree cookie-consent__agree">{{ trans('cookieConsent::texts.disagree') }}</button>--}}
             </div>
         </div>
     </div>
@@ -25,3 +32,13 @@
         font-size: 24px;
     }
 </style>
+
+<script>
+    let closeButton = document.querySelector('.close');
+
+    closeButton.addEventListener("click", () => {
+        console.log('hi')
+        let cookieBanner = document.querySelector('.js-cookie-consent')
+        cookieBanner.style.display = "none";
+    })
+</script>
